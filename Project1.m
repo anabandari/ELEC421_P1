8a46601ee8905ce7a6fb2a393377c69f597ff1e9
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2025-10-26 23:01:02 -0700

    Part 2 WIP

--- Project1.m
+++ Project1.m
@@ -29,3 +29,26 @@ plot(samples, extimecomp);
 hold off
 
 clc;
+
+%%%%%%%%%%%%%%%%%%%%%
+% Part 2 - Decimation-in-Time (DIT) FFT Implementation
+%%%%%%%%%%%%%%%%%%%%%
+function twid = Twiddle(N)
+    twid = zeros(1,N);
+    for k = 0:N-1
+        twid(k+1) = exp(-2*pi*1i*k/N); % Compute the twiddle factors
+    end
+end 
+
+function decintime = dit(ditsignal)
+    ditodd = ditsignal(2:2:end);
+    diteven = ditsignal(1:2:end);
+    twiddlehalf = Twiddle(size(ditsignal/2));
+    twiddlefull = Twiddle(size(ditsignal));
+    
+    for k=0:size(ditsignal)
+        for r=0:size(ditsignal/2)
+            diteven(r)*Twiddlehalf(r)
+        end 
+    end
+end 
