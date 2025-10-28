565747a2532eef201a1895c8c365092a745aec9d
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2025-10-28 16:55:50 -0700

    ndft changes complete

--- naive_dft.m
+++ naive_dft.m
@@ -1,11 +1,16 @@
 %%%%%%%%%%%%%%%%%%%%%
 % Part 1 - Naive DFT Implementation and Cost Analysis
+%
+% Inputs: A discrete signal in terms of n, Number of samples 'N' 
+% Outputs: The Naive DFT
+% 
 %%%%%%%%%%%%%%%%%%%%%
+
 function ndft = naive_dft(Samples, Signal)
 ndft = zeros(1, Samples); % Initialize the output array
     for k = 1:Samples   
-        for n = 0:Samples
-            ndft(k) = ndft(k) + Signal*exp(-2*pi*1i*k*n/Samples);
+        for n = 1:Sampless
+            ndft(k) = ndft(k) + Signal(n)*exp(-1i*k*(2*pi/Samples)*n);
         end
     end
 end
