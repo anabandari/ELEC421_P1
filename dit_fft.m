442cc0e6eca948ef7eec71a0296afc9ce620595e
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2025-10-28 15:50:24 -0700

    naive_dtf changes

--- dit_fft.m
+++ dit_fft.m
@@ -1,14 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%
 % Part 2 - Decimation-in-Time (DIT) FFT Implementation
 %%%%%%%%%%%%%%%%%%%%%
-function twid = Twiddle(N)
-    twid = zeros(1,N);
-    for k = 0:N-1
-        twid(k+1) = exp(-2*pi*1i*k/N); % Compute the twiddle factors
-    end
-end 
-
-function decintime = dit(ditsignal)
+function decintime = dit_fft(ditsignal)
     ditodd = ditsignal(2:2:end);
     diteven = ditsignal(1:2:end);
     twiddlehalf = Twiddle(size(ditsignal/2));
@@ -18,5 +11,12 @@ function decintime = dit(ditsignal)
         for r=0:size(ditsignal/2)
             diteven(r)*Twiddlehalf(r)
         end 
+    end
+end 
+
+function twid = Twiddle(N)
+    twid = zeros(1,N);
+    for k = 0:N-1
+        twid(k+1) = exp(-2*pi*1i*k/N); % Compute the twiddle factors
     end
 end 
--- naive_dft.m
+++ naive_dft.m
@@ -1,12 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%
 % Part 1 - Naive DFT Implementation and Cost Analysis
 %%%%%%%%%%%%%%%%%%%%%
-function ndft = NaiveDFT(n)
-ndft = zeros(1, n); % Initialize the output array
-for k = 0:n-1
-    for m = 0:n-1
-        ndft(k+1) = ndft(k+1) + exp(-2*pi*1i*k*m/n); % Compute the DFT
+function ndft = naive_dft(Samples, Signal)
+ndft = zeros(1, Samples); % Initialize the output array
+    for k = 1:Samples   
+        for n = 0:Samples
+            ndft(k) = ndft(k) + Signal*exp(-2*pi*1i*k*n/Samples);
+        end
     end
-end
 end
 
