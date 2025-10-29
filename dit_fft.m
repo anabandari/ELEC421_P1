e8a782f13ea9f6f1531af29b0a6064d3404f7258
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2025-10-28 17:09:41 -0700

    work in progress

--- dit_fft.m
+++ dit_fft.m
@@ -1,15 +1,16 @@
 %%%%%%%%%%%%%%%%%%%%%
 % Part 2 - Decimation-in-Time (DIT) FFT Implementation
 %%%%%%%%%%%%%%%%%%%%%
-function decintime = dit_fft(ditsignal)
-    ditodd = ditsignal(2:2:end);
-    diteven = ditsignal(1:2:end);
-    twiddlehalf = Twiddle(size(ditsignal/2));
-    twiddlefull = Twiddle(size(ditsignal));
+function decintime = dit_fft(Signal, Samples)
+    decintime = zeros(1, Samples);
+    ditodd = Signal(2:2:Samples);
+    diteven = Signal(1:2:Samples);
+    twiddlehalf = Twiddle(Samples/2);
+    twiddlefull = Twiddle(Samples);
     
-    for k=0:size(ditsignal)
-        for r=0:size(ditsignal/2)
-            diteven(r)*Twiddlehalf(r)
+    for k=1:Samples
+        for r=1:(Samples/2)
+            decintime(k) = decintime(k) + diteven(r)*twiddlehalf^(r*k) + twiddlefull^(k)*ditodd(r)*twiddlehalf^(r*k);
         end 
     end
 end 
