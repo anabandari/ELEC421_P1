b3621674ea8f036199b977d405a07eb635d0edf8
Suchir Srivastava <suchir0311@gmail.com>
2025-11-01 18:04:13 -0700

    implemented DIF FFT algorithm

--- dif_fft.m
+++ dif_fft.m
@@ -2,50 +2,21 @@
 % Part 3 - Decimation-in-Frequency (DIF) FFT Implementation
 %%%%%%%%%%%%%%%%%%%%%
 
-% x_sample = [ones(5,  1)]; % sample signal for now
-x_sample = [5,3,2,1,0,0].';
 
-% declare global counter value to use in function
-global counter;
-counter = 0;
-
-% call function
-[X_k, X_k_bit_reversed, X_k_FFT, DIF_diff, t] = dif_fft_func(x_sample);
-
-
-% FUNCTION: Takes DIT FFT of discrete input signal
+% FUNCTION: Takes DIF FFT of discrete input signal
 % param: x_n, discrete input signal
-% return: 
-%   X_k, DIF FFT discrete output signal
-%   X_k_bit_reversed, bit reversed DIF FFT discrete output signal
-%   X_k_fft, MATLAB FFT discrete output signal
-%   DIF_diff, difference between DIF FFT and MATLAB FFT outputs
-function [X_k, X_k_bit_reversed, X_k_FFT, DIF_diff, t] = dif_fft_func(x_n)
-tic;
+% return: X_k, DIF FFT discrete output signal
+function X_k = dif_fft(x_n)
 
-global counter;
-if (counter == 0) % only check on first recursive loop
-    x_n_len = length(x_n);
+x_n_len = length(x_n);
 
-    % check that signal is a power of 2
-    % if not, zero pad x[n] to power of 2
-    if (mod(x_n_len, log2(x_n_len)) ~= 0)
-        next_exp_2 = ceil(log2(x_n_len));
-        zeros_needed = [zeros(2^next_exp_2 - x_n_len, 1)];
-        x_n = vertcat(x_n, zeros_needed);
-    end
-
-    % check that input signal is not empty
-    if (x_n_len == 0)
-        error('Input signal is empty!')
-    end
-
-    % increment counter only once so if block doesn't run again
-    counter = counter + 1;
+if (mod(x_n_len, log2(x_n_len)) ~= 0)
+    next_exp_2 = ceil(log2(x_n_len));
+    zeros_needed = [zeros(2^next_exp_2 - x_n_len, 1)];
+    x_n = vertcat(x_n, zeros_needed);
 end
 
-
-% check for base case N = 1
+% check for recursion base case N = 1
 if (length(x_n) == 1)
     X_k = x_n;
     return 
@@ -66,20 +37,17 @@ n = (0:N/2-1);
 w_N = exp(-1j * n * 2 * pi / N).'; % convert to column vector
 
 % 3.b multiply
-X_odd = X_odd .* w_N; % multiplication by element in matlab
+X_odd = X_odd .* w_N; % multiplication by element in MATLAB
 
 % Recursion
-X_even = dif_fft_func(X_even);
-X_odd = dif_fft_func(X_odd);
+X_even = dif_fft(X_even);
+X_odd = dif_fft(X_odd);
 
 % Compile results
 X_k = [X_even; X_odd];
 
-t = toc;
-
-X_k_bit_reversed = bitrevorder(X_k);
-X_k_FFT = fft(x_n);
-DIF_diff = abs(X_k_bit_reversed) - abs(X_k_FFT);
+% Note: bit reversal is not done within this function but is applied when
+% the function is invoked in the main project1.m file
 
 end
 
